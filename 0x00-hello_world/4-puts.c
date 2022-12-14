cc6c46744924508a4ac360812c11306c47d8866a
sharon-tissia <sharontissia@gmail.com>
2022-12-14 07:53:08 -0800

    Hot Fix

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
