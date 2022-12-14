8684dd0602b161186e6285f429227ffc169f7974
sharon-tissia <sharontissia@gmail.com>
2022-12-14 01:00:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function  
+ * main - A program that prints a line with puts function
  * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle\n");
 return (0);
 }
