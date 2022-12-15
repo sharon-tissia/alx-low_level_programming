63324f3976624cd2ffc4d2eca99edaf28c7b64ed
sharon-tissia <sharontissia@gmail.com>
2022-12-15 01:26:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - Entry point
  * Return: Always 1
  */
-int main(void) 
+int main(void)
 {
-       	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 61);
 	return (1);
-} 
+}
