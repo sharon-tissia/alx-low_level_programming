d24df80425da5fb1180cac3824aed672295dae5a
sharon-tissia <sharontissia@gmail.com>
2022-12-17 05:01:58 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
+#include "main.h"
 /**
- * main - print _putchar
- * Written by sharon-tissia
- * Return: 0
+ * main - point _putchar
+ * written by sharon-tissia
+ * Return: 0 (success)
  */
 int main(void)
-
 {
-_putchar ('_');
-_putchar ('p');
-_putchar ('u');
-_putchar ('t');
-_putchar ('c');
-_putchar ('h');
-_putchar ('a');
-_putchar ('r');
-_putchar ('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
