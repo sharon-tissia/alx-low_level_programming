7942f77a0cfd7aacb9a3cb0fbbf0812fd45ed0a2
sharon-tissia <sharontissia@gmail.com>
2022-12-22 00:04:00 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,13 @@ int main(void)
 {
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
+	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
-
+	{
 		putchar(letter);
-	putchar('\n');
+	}
+		putchar('\n');
 	return (0);
 }
