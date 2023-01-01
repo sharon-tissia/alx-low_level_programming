5e0518fe391b5734771e65c267c46dc24796e306
sharon-tissia <sharontissia@gmail.com>
2022-12-31 22:35:07 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include <main.h>
+
 /**
  * main - check the code.
  * Return: Always 0.
  */
 int main(void)
 {
-	times_table(3);
+	print_times_table(3);
 	_putchar('\n');
 	print_times_table(5);
 	_putchar('\n');
