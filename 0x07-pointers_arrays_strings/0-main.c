d3eb372286ce03a649bad0fd77f6c963f915849d
sharon-tissia <sharontissia@gmail.com>
2023-01-04 15:08:07 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,22 +10,22 @@ void simple_print_buffer(char *buffer, unsigned int size)
 {
 	unsigned int i;
 	i = 0;
-
-	while (i < size)
-	{
-		if (i % 10)
+		while (i < size)
 		{
-			printf(" ");
+			if (i % 10)
+			{
+				printf(" ");
+			}
+			if (!(i % 10) && i)
+			{
+				printf("\n");
+			}
+			printf("0x%02x", buffer[i]);
+			i++;
 		}
-		if (!(i % 10) && i)
-	}
-	printf("\n");
+		printf("\n");
 }
-printf("0x%02x", buffer[i]);
-i++;
-}
-printf("\n");
-}
+
 /**
  * main - check the code
  * Return: Always 0.
@@ -33,7 +33,6 @@ printf("\n");
 int main(void)
 {
 	char buffer[98] = {0x00};
-
 	simple_print_buffer(buffer, 98);
 	_memset(buffer, 0x01, 95);
 	printf("-------------------------------------------------\n");
